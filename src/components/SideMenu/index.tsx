4c10e4fe66ed63b2d6c18b6af90375ea0e71defb
Célio V. Júnior <celio_cjr@hotmail.com>
2023-03-25 15:57:13 -0300

    dev: building side bar and set scroll effects

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react"
 import { Container } from "./styles"
 
-export default function SideMenu({ children }) {
+const scrollThreshold = 300;
+
+export default function SideMenu({ children = '' }) {
+  const [scrollY, setScrollY] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      setScrollY(window.scrollY)
+      setIsActive(false)
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  const classes = [
+    isActive ? 'open' : '',
+    scrollY <= scrollThreshold ? 'scrollOpen' : '',
+  ];
+
+  const className = classes.join(' ').trim();
+
   return(
-    <Container>
+    <Container className={className}>
       {children}
     </Container>
   )
--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -11,10 +11,28 @@ export const Container = styled.div`
 
   @media (min-width: 1024px) {
     width: calc(100% / 3);
-    box-shadow: -7px 0px -6px rgba(0,0,0,0.75);
+    box-shadow: -7px 0px -6px rgba(0,0,0,0.075);
   }
 
   @media (min-width: 1440px) {
     width: calc(480px + ((100vw - 1440px) / 2));
   }
+
+  transform: translateX(100%);
+  transition: 0.5s transform cubic-bezier(0.5, 0, 0, 1);
+
+  &.open {
+    transform: translateX(0);
+  }
+  
+  @media (min-width: 1024px) {
+    &.scrollOpen {
+      transform: translateX(0);
+    }
+
+    &.scrollOpen .action-close {
+      display: none;
+    }
+  }
 `
+
